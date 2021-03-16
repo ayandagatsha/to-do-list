a657f548bd4d089a05497fdc1ca24ac6c9f64e34
ayanda <64249187+ayandagatsha@users.noreply.github.com>
2021-03-16 11:13:02 +0400

    v1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,37 @@
 import React from "react";
 
 function App() {
+  const [listItem, setListItem] = React.useState();
+  let arrayOfItems = [];
+
+  function listItemSaved(event) {
+    const { value, name } = event.target;
+
+    const listItemForArray = { [name]: value };
+    const item = listItemForArray.listItemName;
+    setListItem(item);
+  }
+
+  function setArrayOfItems() {
+    console.log(listItem);
+    arrayOfItems.push(listItem);
+    console.log(arrayOfItems);
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={listItemSaved} type="text" name="listItemName" />
+        <button onClick={setArrayOfItems}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          <li>{arrayOfItems}</li>
         </ul>
       </div>
     </div>
